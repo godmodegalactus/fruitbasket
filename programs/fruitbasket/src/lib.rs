818379953e9179d9bec8bcd27506946cd8ce2828
godemodegalactus <godmodegalactus@gmail.com>
2022-01-08 23:25:59 +0100

    implementing initialize group and add tokens

--- programs/fruitbasket/src/lib.rs
+++ programs/fruitbasket/src/lib.rs
@@ -1,11 +1,15 @@
 use anchor_lang::prelude::*;
 use std::{mem::size_of, str::Utf8Error};
+use anchor_spl::token::{self, Token, SetAuthority, TokenAccount};
+use spl_token::instruction::{AuthorityType};
+use spl_token::instruction::{initialize_account};
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
-const MAX_NB_TOKENS : usize = 100;
+const MAX_NB_TOKENS : usize = 20;
 const MAX_NB_COMPONENTS: usize = 10;
 const FRUIT_BASKET_GROUP : &[u8] = b"fruitbasket_group";
 const FRUIT_BASKET_CACHE : &[u8] = b"fruitbasket_cache";
+const FRUIT_BASKET_AUTHORITY : &[u8] = b"fruitbasket_auth";
 
 #[program]
 pub mod fruitbasket {
@@ -15,23 +19,47 @@ pub mod fruitbasket {
             _bump_group: u8, 
             _bump_cache: u8,
             base_mint: Pubkey,
-            base_mint_name: String,
-            token_data: Vec<TokenInfo>) -> ProgramResult {
-        assert!( token_data.len() < MAX_NB_TOKENS );
-        
+            base_mint_name: String) -> ProgramResult {
+        // init cache
+        ctx.accounts.cache.load_init()?;
+        // init gr
         let mut group = ctx.accounts.fruit_basket_grp.load_init()?;
         group.owner = *ctx.accounts.owner.key;
-        group.token_count = token_data.len() as u8;
+        group.token_count = 0;
         group.base_mint = base_mint;
-        let mint_name : &[u8] = base_mint_name[..].as_bytes();
-        group.base_mint_name[..8].clone_from_slice(mint_name);
+        let size : usize = if base_mint_name.len() > 10 {10} else {base_mint_name.len()} ;
+        let mint_name : &[u8] = base_mint_name[..size].as_bytes();
+        group.base_mint_name[..size].clone_from_slice(mint_name);
         group.number_of_baskets = 0;
-        let i = 0;
-        for token in token_data.iter() {
-            group.token_mints[i] = token.mint;
-            group.oracles[i] = token.oracle;
-            group.token_names[10*i..10*(i+1)].clone_from_slice(&token.name);
+        Ok(())
+    }
+
+    pub fn add_token(ctx: Context<AddToken>, name: String) -> ProgramResult {
+        let mut group = ctx.accounts.fruit_basket_grp.load_mut()?;
+        let current : usize = group.token_count as usize;
+        assert!(current < MAX_NB_TOKENS);
+        group.token_mints[current] = *ctx.accounts.mint.key;
+        group.oracles[current] = *ctx.accounts.oracle.key;
+        let size : usize = if name.len() > 10 {10} else {name.len()};
+        group.token_names[10*current..10*(10*current + size)].clone_from_slice(&name[..size].as_bytes());
+        if size < 10 {
+            group.token_names[10*current + size + 1] = 0;
         }
+        let (authority, _bump) = Pubkey::find_program_address(&[FRUIT_BASKET_AUTHORITY], ctx.program_id);
+        {
+            // change authority of token pool to authority            
+            let cpi_accounts = SetAuthority {
+                account_or_mint: ctx.accounts.token_pool.to_account_info().clone(),
+                current_authority: ctx.accounts.owner.clone(),
+            };
+            let cpi_program = ctx.accounts.token_program.to_account_info();
+            let cpi =  CpiContext::new(cpi_program, cpi_accounts);
+            token::set_authority(cpi, AuthorityType::AccountOwner, Some(authority))?;
+        }
+        group.token_pools[current] = *ctx.accounts.token_pool.to_account_info().key;
+
+        group.token_count = group.token_count + 1;
+        //group.token
         Ok(())
     }
 }
@@ -39,7 +67,7 @@ pub mod fruitbasket {
 #[derive(Accounts)]
 #[instruction(bump_group: u8, bump_cache: u8)]
 pub struct InitializeGroup<'info> {
-    #[account(signer)]
+    #[account(mut, signer)]
     owner : AccountInfo<'info>,
 
     #[account( init,
@@ -51,12 +79,29 @@ pub struct InitializeGroup<'info> {
 
     #[account( init,
         seeds = [FRUIT_BASKET_CACHE, &owner.key.to_bytes()],
-        bump = bump_group, 
+        bump = bump_cache, 
         payer = owner, 
         space = 8 + size_of::<Cache>() )]
     cache : AccountLoader<'info, Cache>,
 
     system_program : Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct AddToken<'info>{
+    #[account(signer)]
+    owner : AccountInfo<'info>,
+
+    #[account(mut)]
+    fruit_basket_grp : AccountLoader<'info, FruitBasketGroup>,
+
+    mint : AccountInfo<'info>,
+    oracle : AccountInfo<'info>,
+    #[account(mut, 
+              constraint = token_pool.owner == *owner.key,
+              constraint = token_pool.mint == *mint.key)]
+    token_pool : Account<'info, TokenAccount>,
+    token_program : Program<'info, anchor_spl::token::Token>,
 }
 
 /// Fruit basket group
@@ -65,37 +110,30 @@ pub struct InitializeGroup<'info> {
 #[account(zero_copy)]
 pub struct FruitBasketGroup {
     pub owner: Pubkey,              // owner
-    pub token_count: u8,           // number of tokens that can be handled
+    pub token_count: u8,            // number of tokens that can be handled
     pub base_mint: Pubkey,          // usdc public key
     pub base_mint_name : [u8; 10],  // name of base / USDC
-    pub token_mints: [Pubkey; MAX_NB_TOKENS], // token mints
-    pub oracles: [Pubkey;MAX_NB_TOKENS],      // oracle keys
-    pub token_names: [u8; MAX_NB_TOKENS*10],
-    pub number_of_baskets : u32,
+    pub token_mints: [Pubkey; 20],  // token mints
+    pub oracles: [Pubkey;20],       // oracle keys
+    pub token_names: [u8; 20],      // token names
+    pub number_of_baskets : u32,    // number of baskets currenly created
+    pub token_pools : [Pubkey; 20], // pool for each token  
 }
 
 #[account()]
 pub struct Basket {
     pub basket_name: [u8; 128],
     pub number_of_components: u8,    // basket description
-    pub components : [u8; MAX_NB_COMPONENTS], // components by index in the fruit basket group
-    pub component_size : [u64; MAX_NB_COMPONENTS],
+    pub components : [u8; 10], // components by index in the fruit basket group
+    pub component_size : [u64; 10],
 }
 
 #[account(zero_copy)]
 pub struct Cache {
-    pub last_price: [u64; MAX_NB_TOKENS],
-    pub last_exp: [u8; MAX_NB_TOKENS],
-    pub last_confidence: [u32; MAX_NB_TOKENS],
+    pub last_price: [u64; 20],
+    pub last_exp: [u8; 20],
+    pub last_confidence: [u32; 20],
 }
-
-#[derive(AnchorSerialize, AnchorDeserialize, Default, Clone, Copy)]
-pub struct TokenInfo {
-    name : [u8;10],
-    mint : Pubkey,
-    oracle : Pubkey,
-}
-
 
 impl<'info> FruitBasketGroup {
     pub fn get_token_name( &self, token_index : usize) -> Result<&str, Utf8Error> {
--- tests/fruitbasket.ts
+++ tests/fruitbasket.ts
@@ -1,17 +1,73 @@
 import * as anchor from '@project-serum/anchor';
-import { Program } from '@project-serum/anchor';
+import { Program, web3 } from '@project-serum/anchor';
 import { Fruitbasket } from '../target/types/fruitbasket';
+import {
+  NATIVE_MINT,
+  Token,
+  TOKEN_PROGRAM_ID,
+  AccountLayout as TokenAccountLayout,
+  u64,
+} from '@solana/spl-token';
+
+import * as testutils from './utils/testutils';
+import { token } from '@project-serum/anchor/dist/cjs/utils';
+import mlog from 'mocha-logger';
+
+type Connection = web3.Connection;
 
 describe('fruitbasket', () => {
 
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+  const connection = provider.connection;
+  const wallet = provider.wallet;
+  //configure test utils
+  const test_utils = new testutils.TestUtils(connection, provider.wallet);
 
   const program = anchor.workspace.Fruitbasket as Program<Fruitbasket>;
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+  const owner = web3.Keypair.generate();
+
+  // create some tokens
+  const usdc = test_utils.createToken(6, wallet.publicKey);
+  const btc = test_utils.createToken(6, wallet.publicKey);
+  const eth = test_utils.createToken(6, wallet.publicKey);
+  const sol = test_utils.createNativeToken();
+  const alt1 = test_utils.createToken(6, wallet.publicKey);
+  const alt2 = test_utils.createToken(6, wallet.publicKey);
+
+  const mint_authority = web3.Keypair.generate();
+
+  let frt_bsk_group = null;
+  let frt_bsk_cache = null;
+  it('Group initialized', async () => {
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(owner.publicKey, 10000000000),
+      "confirmed"
+    );
+
+    const [tmp_group, bump_grp] = await web3.PublicKey.findProgramAddress([Buffer.from('fruitbasket_group'), owner.publicKey.toBuffer()], program.programId);
+    const [tmp_cache, bump_cache] = await web3.PublicKey.findProgramAddress([Buffer.from('fruitbasket_cache'), owner.publicKey.toBuffer()], program.programId);
+
+    frt_bsk_group = tmp_group;
+    frt_bsk_cache = tmp_cache;
+    mlog.log("group : " + frt_bsk_group);
+    mlog.log("cache : " + frt_bsk_cache);
+    await program.rpc.initializeGroup(
+      bump_grp,
+      bump_cache,
+      (await usdc).publicKey,
+      "usdc",
+      {
+        accounts:{
+          owner: owner.publicKey,
+          fruitBasketGrp: frt_bsk_group,
+          cache: frt_bsk_cache,
+          systemProgram: web3.SystemProgram.programId,
+        },
+        signers:[owner]
+      }
+    );
   });
 });
